24f990e4180d1d4732aa2644fe85a831fbdde049
Prashan070 <prashansrivastava77@gmail.com>
2024-11-06 23:46:58 +0530

    Time: 668 ms (17%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,4 @@
-/* Write your T-SQL query statement below */
-SELECT NAME FROM CUSTOMER WHERE REFEREE_ID != 2 
-or REFEREE_ID IS NULL;
+
+Select name
+From Customer
+where IFNULL (referee_id, -1) <> 2;
