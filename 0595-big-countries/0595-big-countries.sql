c56495700ff8b7e8cce320bf3487995014a9673f
Prashan070 <prashansrivastava77@gmail.com>
2024-11-07 00:13:52 +0530

    Update 0595-big-countries.sql

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -4,3 +4,15 @@ FROM World
 WHERE area >= 3000000 
 OR
 population  >= 25000000;
+
+------------------------------
+
+Select name, population, area
+FROM World
+WHERE area >= 3000000 
+
+UNION
+
+Select name, population, area
+FROM World
+WHERE population  >= 25000000;
